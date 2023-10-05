5069b0434e67f0b078431f8b812dd28bf904a9b0
Ryan J Field <RyanJField@users.noreply.github.com>
2023-10-05 16:47:50 +0100

    Updates

--- R/utils.R
+++ R/utils.R
@@ -32,3 +32,60 @@ data_wide_to_long <- function(df) {
 is_wide <- function(df) {
   return(any(grepl("(.+)\\.(\\d+)", names(df))))
 }
+
+get_all_components <- function(components_column) {
+  components <- levels(as.factor(components_column))
+  components <- paste(components, collapse = "+")
+  components <- strsplit(components, "\\+")[[1]]
+  # Convert to factor (for speed)
+  components <- as.factor(components)
+}
+
+get_unique_components <- function(components_column) {
+  components <- get_all_components(components_column)
+  return(
+    # Use levels to get unique components
+    components <- levels(components)
+  )
+}
+
+get_most_frequent_component <- function(components_column) {
+  components <- get_all_components(df$Components)
+  component_frequency <- data.frame(table(components))
+  most_frequent_component <-
+    component_frequency[which.max(component_frequency$Freq), ]$components
+  return(most_frequent_component)
+}
+
+format_data <- function(df, control = NULL) {
+  # Convert columns to lowercase
+  names(df) <- tolower(names(df))
+  n_trials <- length(levels(as.factor(df$study)))
+  if (is.null(control)) {
+    control <- get_most_frequent_component(df$components)
+  }
+
+}
+
+format_data_binary <- function(df) {
+
+}
+
+format_data_continous <- function(df) {
+
+}
+
+get_n_arms <- function(df) {
+  require(dplyr)
+  tmp_data <- df
+  tmp_data <- tmp_data %>%
+    dplyr::mutate(study = as.factor(study)) %>%
+    dplyr::group_by(study) %>%
+    dplyr::mutate(n = n()) %>%
+    dplyr::select(study, n) %>%
+    dplyr::filter(row_number() == 1)
+}
+
+get_n_array <- function(data, control, column = "events") {
+
+}
