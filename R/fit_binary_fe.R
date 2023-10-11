c5e6952967d4c1e5f11adba7a78d178d64546f67
Ryan J Field <RyanJField@users.noreply.github.com>
2023-10-11 17:07:35 +0100

    Updates

--- R/fit_binary_fe.R
+++ R/fit_binary_fe.R
@@ -1,4 +1,14 @@
-fit_binary_fe <- function(data, control = NULL) {
+fit_binary_fe <- function(
+  data,
+  control_component = NULL,
+  chains = 3,
+  warmup = 500,
+  iter = 1500,
+  seed = 12345,
+  max_treedepth = 10,
+  adapt_delta = 0.95,
+  stepsize = 0.01
+) {
   if (! "study" %in% tolower(names(data))) {
     stop("The column study is missing from the data")
   }
@@ -8,4 +18,19 @@ fit_binary_fe <- function(data, control = NULL) {
   }
   validate_columns(df, "binary")
 
+  stan_data <- format_data(df, "binary", control_component)
+  stan_fit <- rstan::stan(
+    file = system.file("models", "binary_FE.stan", package = "MetaCNMABayes"),
+    data = stan_data,
+    chains = chains,
+    warmup = warmup,
+    iter = iter,
+    seed = seed,
+    control = list(
+      max_treedepth = max_treedepth,
+      adapt_delta = adapt_delta,
+      stepsize = stepsize
+    )
+  )
+  return(stan_fit)
 }
--- R/utils.R
+++ R/utils.R
@@ -39,6 +39,7 @@ get_all_components <- function(components_column) {
   components <- strsplit(components, "\\+")[[1]]
   # Convert to factor (for speed)
   components <- as.factor(components)
+  return(components)
 }
 
 get_unique_components <- function(components_column) {
@@ -49,51 +50,175 @@ get_unique_components <- function(components_column) {
   )
 }
 
+get_comps_no_control_component <-
+  function(components_column, control_component = NULL) {
+    if (is.null(control_component)) {
+      control_component <- get_most_frequent_component(components_column)
+    }
+    tmp_components <- get_unique_components(components_column)
+    tmp_components <- tmp_components[!tmp_components == control_component]
+    return(tmp_components)
+  }
+
+get_single_components <- function(components_column) {
+  tmp_components <- components_column
+  tmp_components <- tmp_components[!grepl("\\+", tmp_components)]
+}
+
+get_n_components <- function(components_column) {
+  length(get_unique_components(components_column)) - 1
+}
+
 get_most_frequent_component <- function(components_column) {
-  components <- get_all_components(df$Components)
+  components <- get_single_components(components_column)
   component_frequency <- data.frame(table(components))
   most_frequent_component <-
     component_frequency[which.max(component_frequency$Freq), ]$components
   return(most_frequent_component)
 }
 
-format_data <- function(df, control = NULL) {
-  # Convert columns to lowercase
-  names(df) <- tolower(names(df))
-  # Require dplyr for magnittr pipe (%>%)
+sort_data <- function(df) {
   require(dplyr)
-  # Group data by study (as a factor)
-  # ensuring data is in the same order for each function
-  df <- df %>%
+  tmp_df <- df
+  names(tmp_df) <- tolower(names(tmp_df))
+  tmp_df <- tmp_df %>%
     dplyr::mutate(study = as.factor(study)) %>% # nolint: object_usage
-    dplyr::group_by(study)
-  n_trials <- length(levels(as.factor(df$study)))
-  if (is.null(control)) {
-    control <- get_most_frequent_component(df$components)
+    dplyr::group_by(study) %>%
+    dplyr::mutate(study_id = dplyr::cur_group_id())
+  return(tmp_df)
+}
+
+format_data <- function(df, data_type, control_component = NULL) {
+  df <- sort_data(df)
+  n_trials <- get_n_trials(df)
+  if (is.null(control_component)) {
+    control_component <- get_most_frequent_component(df$components)
   }
+  n_arms <- get_n_arms(df)
+  n_components <- get_n_components(df$components)
+  if (data_type == "binary") {
+    return(format_data_binary(
+      df,
+      n_trials,
+      n_arms, n_components,
+      control_component
+    ))
+  } else {
+    return(format_data_continous(
+      df,
+      n_trials,
+      n_arms,
+      n_components,
+      control_component
+    ))
+  }
+}
+
+format_data_binary <- function(
+  df,
+  n_trials,
+  n_arms,
+  n_components,
+  control_component = NULL
+) {
+  tmp_df <- sort_data(df)
+  r <- get_n_array(tmp_df)
+  n <- get_n_array(tmp_df, column = "total")
+  components <- get_component_array(df, control_component)
+  return(
+    list(
+      n_trials = n_trials,
+      n_arms = n_arms,
+      n_components = n_components,
+      n = n,
+      r = r,
+      components = components
+    )
+  )
+}
+
+format_data_continous <- function(
+  df,
+  n_trials,
+  n_arms,
+  n_components,
+  control_component = NULL
+) {
 
 }
 
-format_data_binary <- function(df) {
-
+get_n_trials <- function(df) {
+  tmp_df <- sort_data(df)
+  return(length(levels(as.factor(tmp_df$study))))
 }
 
-format_data_continous <- function(df) {
-
+get_studies <- function(df) {
+  tmp_df <- sort_data(df)
+  return(levels(tmp_df$study))
 }
 
 get_n_arms <- function(df) {
   require(dplyr)
-  tmp_data <- df
+  tmp_data <- sort_data(df)
   tmp_data <- tmp_data %>%
-    dplyr::mutate(study = as.factor(study)) %>%
-    dplyr::group_by(study) %>%
     dplyr::mutate(n = n()) %>%
     dplyr::select(study, n) %>%
     dplyr::filter(row_number() == 1)
   return(as.numeric(tmp_data$n))
 }
 
-get_n_array <- function(data, control, column = "events") {
+get_n_array <- function(df, control_component = NULL, column = "events") {
+  require(dplyr)
+  tmp_df <- sort_data(df)
+  tmp_df <- tmp_df %>%
+    dplyr::mutate(study = as.factor(study)) %>% # nolint: object_usage
+    dplyr::group_by(study) %>%
+    dplyr::mutate(study_id = dplyr::cur_group_id())
+  n_trials <- get_n_trials(tmp_df)
+  max_arms <- max(get_n_arms(tmp_df))
+  n_array <- array(NA, dim = c(n_trials, max_arms))
+  if (is.null(control_component)) {
+    control_component <- get_most_frequent_component(tmp_df$components)
+  }
+  for (study_id in 1:max(tmp_df$study_id)){
+    study <- tmp_df[tmp_df$study_id == study_id, ]
+    study_control_component <- study[study$components == control_component, ]
+    study_arms <- study[!study$components == control_component, ]
+    # Set control_component
+    n_array[study_id, 1] <- study_control_component[[column]]
+    for (arm in seq_len(nrow(study_arms))) {
+      n_array[study_id, (arm + 1)] <- study_arms[arm, ][[column]]
+    }
+  }
+  return(n_array)
+}
 
+get_component_array <- function(df, control_component = NULL) {
+  tmp_df <- sort_data(df)
+  n_components <- get_n_components(tmp_df$components)
+  n_trials <- get_n_trials(tmp_df)
+  max_arms <- max(get_n_arms(tmp_df))
+  if (is.null(control_component)) {
+    control_component <- get_most_frequent_component(tmp_df$components)
+  }
+  components <- get_comps_no_control_component(tmp_df$components)
+  component_array <- array(NA, dim = c(n_components, n_trials, max_arms))
+  for(component in seq_len(n_components)){
+    tmp_component <- components[component]
+    print(tmp_component)
+    for(study_id in seq_len(max(tmp_df$study_id))) {
+      component_array[component, study_id, 1] <- 0
+      tmp_study <- tmp_df[tmp_df$study_id == study_id, ]
+      tmp_study <- tmp_study[!tmp_study$components == control_component, ]
+      for (arm in seq_len(nrow(tmp_study))) {
+        tmp_study_components <- get_all_components(tmp_study[arm, ]$components)
+        if (tmp_component %in% tmp_study_components) {
+          component_array[component, study_id, (arm + 1)] <- 1
+        } else {
+          component_array[component, study_id, (arm + 1)] <- 0
+        }
+      }
+    }
+  }
+  return(component_array)
 }
